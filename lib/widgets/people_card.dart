70eacb4ebe953acc7f2930401353804a92369b87
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-08-05 19:43:44 -0400

    Create people card Widget

---
+++ lib/widgets/people_card.dart
@@ -0,0 +1,22 @@
+import 'package:basic/models/people.dart';
+import 'package:flutter/material.dart';
+
+class PeopleCard extends StatelessWidget{
+
+  People people;
+
+  PeopleCard(this.people);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Column(
+        children: <Widget>[
+          Text(people.name),
+          Text(people.number),
+        ],
+      ),
+    );
+  }
+
+}
