62cf559d12528032b387b50f8a2e240a1e777db9
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-08-05 22:27:22 -0400

    Create Textfield Widget from input data

---
+++ lib/widgets/add_input.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AddInput extends StatelessWidget{
+  TextEditingController controller;
+  bool _validate;
+  String _hintText;
+  String _errorText;
+  Icon _icon;
+  FocusNode passFocus;
+
+  AddInput(this.controller, this._validate, this._hintText, this._errorText, this._icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.black,
+        ),
+        onSubmitted: (String valor){
+          FocusScope.of(context).requestFocus(passFocus);
+        },
+        controller: controller,
+        decoration: InputDecoration(
+          border: new OutlineInputBorder(
+              borderSide: new BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.all(Radius.circular(30.0))
+          ),
+          icon: _icon,
+          errorText: _validate ? _errorText: null,
+          hintText: _hintText,
+        ),
+
+      ),
+    );
+  }
+
+}
